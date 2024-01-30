04e7972939c585139a56ab0916992768d1b85655
dean-moisakos <Konstantinos-Moisakos@mohawkcollege.ca>
2024-01-30 10:11:02 -0500

    trained model deployed

--- pill_identifier.py
+++ pill_identifier.py
@@ -1,5 +1,28 @@
 from ultralytics import YOLO
+import cv2 as cv
 
-model = YOLO('models/pill.pt')
+#paths
+tracker_path = 'data/botsort.yaml'
+model_path = 'models/pill.pt'
 
-results = model(source=0, show=True, conf=0.3, tracker='botsort.yaml')
+#initialize model
+model = YOLO(model_path)
+
+#Visualize results
+results = model(source=0, show=True, conf=0.3, tracker=tracker_path)
+
+cap = cv.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    results = model.track(frame, tracker=tracker_path)
+    print(results)
+
+    for result in results:
+        print(result)
+        
+   
+    cv.imshow('Pill Identification', frame)
+
+    if cv.waitKey(1) > 0:
+        break 
