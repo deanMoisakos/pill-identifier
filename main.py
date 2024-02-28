2a7bad38ffd56b31028aab5b0d5885e35e485924
dean-moisakos <Konstantinos-Moisakos@mohawkcollege.ca>
2024-02-28 15:27:02 -0500

    main script to run program

---
+++ main.py
@@ -0,0 +1,6 @@
+from tools.pill import PillIdentifier
+
+if __name__ == "__main__":
+    pill_video_2 = 'videos/pill_video_2.mp4'
+    pill_identifier = PillIdentifier(pill_video_2)
+    pill_identifier()
