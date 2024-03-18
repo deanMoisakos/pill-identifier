eab22440ef83f70e249f5a3714e86421c119e16f
dean-moisakos <Konstantinos-Moisakos@mohawkcollege.ca>
2024-03-18 09:59:22 -0400

    pill identification and tracking

--- main.py
+++
@@ -1,6 +0,0 @@
-from tools.pill import PillIdentifier
-
-if __name__ == "__main__":
-    pill_video_2 = 'videos/pill_video_2.mp4'
-    pill_identifier = PillIdentifier(pill_video_2)
-    pill_identifier()
