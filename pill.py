df6e37fdfab21cab052d8e5fd51fcc726f12c41f
dean-moisakos <Konstantinos-Moisakos@mohawkcollege.ca>
2024-04-11 14:33:18 -0400

    completed YOLOV8 application

--- pill.py
+++ pill.py
@@ -20,13 +20,12 @@ def video_detection(path_x):
         if not success:
             yield [None, []]
         results=model(img,stream=True)
-        classStrings = []
+        classStrings = ""
         for r in results:
             boxes=r.boxes
             for box in boxes:
                 x1,y1,x2,y2=box.xyxy[0]
                 x1,y1,x2,y2=int(x1), int(y1), int(x2), int(y2)
-                print(x1,y1,x2,y2)
                 cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,255),3)
                 conf=math.ceil((box.conf[0]*100))/100
                 cls=int(box.cls[0])
@@ -40,10 +39,9 @@ def video_detection(path_x):
                 if class_name == 'Tylenol Extra Strength':
                     print_label = f' Tylenol Extra Strength Detected | Purpose: Pain Relief | Dosage: 500mg Acetaminophen'
                 t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
-                print(t_size)
                 c2 = x1 + t_size[0], y1 - t_size[1] - 3
                 cv2.rectangle(img, (x1,y1), c2, [255,0,255], -1, cv2.LINE_AA)  # filled
                 cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=1,lineType=cv2.LINE_AA)
-                classStrings.append(print_label)
+                classStrings = classStrings + "\n" + print_label 
         yield [img, classStrings]
 cv2.destroyAllWindows()
